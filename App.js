f13a3c2f4edc711a45d932a6c7a6eb37ab6dcad0
Akanksha <nehetea@mcmaster.ca>
2023-01-14 15:46:05 -0500

    added gradient

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView , Image} from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
+  console.log("App Executed");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // container component for grouping or laying out children
+    <LinearGradient 
+          colors={["#B9E0FF", "#C4FFF8"]}
+          start={[0.1, 0.1]}
+          style={styles.linearGradient}>
+    <SafeAreaView style={styles.container}>
+      
+      <Text>DashBuddy</Text> 
+      
       <StatusBar style="auto" />
-    </View>
+      
+
+    </SafeAreaView>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, // container is flexible
+    backgroundColor: 'B9E0FF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  linearGradient: {
+    width: '100%',
+    height: '100%',
+    opacity: 0.95,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
